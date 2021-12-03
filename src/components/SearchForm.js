7211b776f14855e7aa80fb6bcdd532c9957dc2f6
François <itoutouberny@gmail.com>
2021-12-03 03:22:27 +0100

    fix(SearchForm.js): add regex verification to submit form event

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,12 +10,18 @@ const SearchForm = (props) => {
         setState(event.target.value)
     }
 
+    const handleSearch = (event) => {
+        event.preventDefault()
+        const testResult = /[0-9]{9}/.test(state)
+        if (testResult){
+            navigate(`/entreprise/${state}`)
+        } else {
+            navigate(`/resultats/${state}/1`)
+        }
+    }
+
     return(
-        <form className={"row"} onSubmit={(event) => {
-            //prevent the submit event from triggering the page reload
-            event.preventDefault()
-            ; navigate(`/resultats/${state}/1`)}
-        }>
+        <form className={"row"} onSubmit={handleSearch}>
             <div className="col-sm-12 col-md-9 my-1 ps-0">
                 <input type="search" className="form-control" id="searchText" placeholder="Entrez un nom, un siret ou un siren" value={state} onChange={handleTextChange}/>
             </div>
@@ -24,7 +30,7 @@ const SearchForm = (props) => {
                     <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"/>
                     <span className={"d-sm-none"}>Recherche en cours</span>
                 </button>}
-                {!loadingState && <button className="btn btn-primary w-100" type="button" onClick={() => navigate(`/resultats/${state}/1`)}>
+                {!loadingState && <button className="btn btn-primary w-100" type="button" onClick={handleSearch}>
                     Lancer
                 </button>}
             </div>
